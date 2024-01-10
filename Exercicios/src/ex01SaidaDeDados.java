3ada3120dc4585eb0270673a7a233d1058e84c9e
Breno Duarte <brenoduarte239@gmail.com>
2024-01-10 19:36:53 -0300

    Organização dos Arquivos

--- Exercicios/src/ex01SaidaDeDados.java
+++ Exercicios/src/ex01SaidaDeDados.java
@@ -1,6 +1,6 @@
 import java.util.Locale;
 
-public class Main {
+public class ex01SaidaDeDados {
 
 	public static void main(String[] args) {
 		
---
+++ Exercicios/src/ex02EntradaDeDados.java
@@ -0,0 +1,9 @@
+
+public class ex02EntradaDeDados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
