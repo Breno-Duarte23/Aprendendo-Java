d5b968eaad1bbdf9cb94cc57f24670056cf8e49c
Breno Duarte <brenoduarte239@gmail.com>
2024-01-12 13:06:57 -0300

    3 exercicios resolivdos

--- Exercicios/src/Ex02CalcBhaskara.java
+++ Exercicios/src/Ex02CalcBhaskara.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Ex02CalcBhaskara {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+/*
+ * 		Bhaskara
 		int a, b, c;
 		Double x1, x2, delta;
 
@@ -26,6 +29,37 @@ public class Ex02CalcBhaskara {
 			System.out.printf("A raiz de x2 é: %.2f %n", x2);
 		}
 
+		//Ex 01 Nelio:
+		int numero1, numero2, resultado;
+		System.out.println("Digite um número inteiro ");
+		numero1 = sc.nextInt();
+		System.out.println("Digite mais um número inteiro ");
+		numero2 = sc.nextInt();
+		resultado = numero1 + numero2;
+		System.out.printf("A soma entre %d e %d e igual a %d", numero1, numero2, resultado);
+		
+		//Ex 02 Nelio:
+		double pi, raio, area;
+		pi = 3.14159;
+		System.out.println("Digite o raio do círculo");
+		raio = sc.nextDouble();
+		area = pi * (Math.pow(raio, 2));
+		System.out.printf("A area de um circulo com raio de %.4f é igual a %.4f", raio, area);
+
+		//Ex 03 Nelio:
+		int a, b, c, d, resultado;
+		System.out.println("Digite um valor inteiro");
+		a = sc.nextInt();
+		System.out.println("Digite mais um valor inteiro");
+		b = sc.nextInt();
+		System.out.println("Digite mais um valor inteiro");
+		c = sc.nextInt();
+		System.out.println("Digite mais um valor inteiro");
+		d = sc.nextInt();
+		resultado = (a * b) - (c * d);
+		System.out.printf("O resultado da diferença entre o produto de %d e %d pelo produto de %d e %d é igual a %d", a, b, c, d, resultado);
+*/
+		
 		sc.close();
 	}
 
