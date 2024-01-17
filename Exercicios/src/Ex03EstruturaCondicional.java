dd01f1fe4a7cfb586a4939cd2b7a4fb9118a4ba1
Breno Duarte <brenoduarte239@gmail.com>
2024-01-17 13:10:57 -0300

    Exercícios de estruturas condicionais

---
+++ Exercicios/src/Ex03EstruturaCondicional.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex03EstruturaCondicional {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+/*		
+		//Ex 01 Nelio:
+		int num;
+		System.out.println("Digite um número inteiro");
+		numPositivo = sc.nextInt();
+		
+		if (numPositivo > 0) {
+			System.out.println("O numero é positivo!");
+		} else {
+			System.out.println("O numero é negativo!");
+		}
+
+		//Ex 02 Nelio:
+		int numPar;
+		System.out.println("Digite um número inteiro");
+		numPar = sc.nextInt();
+		if (numPar % 2 == 0) {
+			System.out.println("Par");
+		} else {
+			System.out.println("Ímpar");
+		}
+*/		
+		//Ex 03 Nelio:
+		int num1, num2;
+		System.out.println("Digite 2 valores inteiros");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		if(num2 % num1 == 0) {
+			System.out.println("São múltiplos");
+		} else if(num1 % num2 == 0) {
+			System.out.println("São múltiplos");
+		} else {
+			System.out.println("Não são múltiplos");
+		}
+		
+		sc.close();
+	}
+
+}
