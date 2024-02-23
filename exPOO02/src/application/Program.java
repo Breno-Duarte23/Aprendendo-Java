e76cbf97fa39f0be9a3ddfb1696d37e7eb0a708e
Breno Duarte <brenoduarte239@gmail.com>
2024-02-22 22:48:41 -0300

    Commit
    ExPoo02

---
+++ exPOO02/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Employee emplo = new Employee();
+		System.out.println("Name: ");
+		emplo.name = sc.nextLine();
+		System.out.println("Gross salary: ");
+		emplo.grossSalary = sc.nextDouble();
+		System.out.println("Tax: ");
+		emplo.tax = sc.nextDouble();
+		
+		System.out.println("Employee: " + emplo);
+		
+		System.out.println("Wich percentage to increase salary? ");
+		double percentage = sc.nextDouble();  
+		emplo.increaseSalary(percentage);
+		
+		System.out.println("Updated data: " + emplo);
+		sc.close();
+	}
+
+}
