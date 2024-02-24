906c97bc4ca1dae5753932968c760314a6b71e6c
Breno Duarte <brenoduarte239@gmail.com>
2024-02-24 20:46:31 -0300

    Commit
    Não solucionado

---
+++ MembrosEstáticos/src/application/Pogram.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Pogram {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("What is the dollar price? ");
+		double price = sc.nextDouble();
+		System.out.println("How many ddollars will be bought? ");
+		double amount = sc.nextDouble();
+		double resultado = 
+		System.out.printf("Ammount to be paid in reais = %.2f ", );
+		
+		sc.close();
+	}
+}
---
+++ MembrosEstáticos/src/util/CurrencyConverter.java
@@ -0,0 +1,14 @@
+package util;
+
+public class CurrencyConverter {
+	public static double dollarPrice;
+	public static double dollarBought;
+	//Métodos
+	public static double iof() {
+		return dollarBought += 6 * 100 / dollarBought;
+	}
+	
+	public  static double converter() {
+		return dollarBought * dollarPrice;
+	}
+}
